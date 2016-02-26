78cd0751cad535f26cd428760616a8e42d058903
Andrew Simmons <agsimmons0@gmail.com>
2016-02-26 12:43:45 -0500

    Create Grid.java

---
+++ src/application/Grid.java
@@ -0,0 +1,24 @@
+package application;
+
+public class Grid {
+	
+	private int width;
+	private int height;
+	private boolean[] grid;
+	
+	public Grid(int width, int height) {
+		this.width = width;
+		this.height = height;
+		grid = new boolean[width * height];
+	}
+	
+	public void setValue(int x, int y) {
+		// TODO
+	}
+	
+	public void update() {
+		// TODO
+	}
+	
+	
+}
