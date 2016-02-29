80619b1b49a25d4d53ff2f8870d81d6753d2fd3a
Andrew Simmons <agsimmons0@gmail.com>
2016-02-29 12:37:00 -0500

    Create method that draws grid state to console

--- src/application/Grid.java
+++ src/application/Grid.java
@@ -4,20 +4,33 @@ public class Grid {
 	
 	private int width;
 	private int height;
-	private boolean[] grid;
+	private boolean[][] grid;
 	
 	public Grid(int width, int height) {
 		this.width = width;
 		this.height = height;
-		grid = new boolean[width * height];
+		grid = new boolean[width][height];
 	}
 	
-	public void setValue(int x, int y) {
-		// TODO
+	public void setTrue(int x, int y) {
+		grid[x][y] = true;
 	}
 	
 	public void update() {
 		// TODO
+	}
+	
+	public void draw() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				if (grid[x][y] == true) {
+					System.out.print("■");
+				} else {
+					System.out.print("□");
+				}
+			}
+			System.out.println();
+		}
 	}
 	
 	
