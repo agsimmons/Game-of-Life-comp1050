95e5470388ad61b45fdb70618dcfe1c18bcd6b40
Andrew Simmons <agsimmons0@gmail.com>
2016-03-16 13:03:07 -0400

    Revert Main.java to original state

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,29 +9,18 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-//		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
-//		launch(args);
-		
-		Grid grid = new Grid(10, 5);
-		grid.changeCellState(2, 1);
-		grid.changeCellState(7, 1);
-		grid.changeCellState(2, 3);
-		grid.changeCellState(3, 4);
-		grid.changeCellState(4, 4);
-		grid.changeCellState(5, 4);
-		grid.changeCellState(6, 4);
-		grid.changeCellState(7, 3);
-		grid.drawState();
+		launch(args);
 	}
 }
