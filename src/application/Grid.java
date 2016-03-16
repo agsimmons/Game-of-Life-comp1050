e6ebdb70257ae87e70d83ee6bae92b77bf473f75
Andrew Simmons <agsimmons0@gmail.com>
2016-03-16 12:45:54 -0400

    Implement getNeighbors method
    Also changed the names of some methods like getCellState and
    simulateCycle

--- src/application/Grid.java
+++ src/application/Grid.java
@@ -1,17 +1,17 @@
 package application;
 
 public class Grid {
-	
+
 	private int width;
 	private int height;
 	private boolean[][] grid;
-	
+
 	public Grid(int width, int height) {
 		this.width = width;
 		this.height = height;
 		grid = new boolean[width][height];
 	}
-	
+
 	public void changeCellState(int x, int y) {
 		if (grid[x][y] == true) {
 			grid[x][y] = false;
@@ -19,11 +19,28 @@ public class Grid {
 			grid[x][y] = true;
 		}
 	}
-	
-	public void update() {
+
+	public void simulateCycle() {
 		// TODO
+//		boolean tempGrid[][] = grid;
+//		
+//		for (int y = 0; y < height; y++) {
+//			for (int x = 0; x < width; x++) {
+//				
+//				if (grid[x][y] == true) { // If cell is alive
+//					// Any live cell with fewer than two live neighbours dies
+//					
+//					// Any live cell with two or three live neighbours lives on to the next generation.
+//					
+//					// Any live cell with more than three live neighbours dies
+//				} else { // If cell is dead
+//					// Any dead cell with exactly three live neighbours becomes a live cell
+//					
+//				}
+//			}
+//		}
 	}
-	
+
 	public void drawState() {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
@@ -36,6 +53,32 @@ public class Grid {
 			System.out.println();
 		}
 	}
-	
-	
+
+	public int getNeighbors(int x, int y) {
+		int count = 0;
+
+		// Check Left
+		if (x > 0) { if (grid[x - 1][y] == true) {count++;} }
+		// Check Top-Left
+		if (x > 0 && y > 0) { if (grid[x - 1][y - 1] == true) {count++;} }
+		// Check Top
+		if (y > 0) { if (grid[x][y - 1] == true) {count++;} }
+		// Check Top-Right
+		if (x < width - 1 && y > 0) { if (grid[x + 1][y - 1] == true) {count++;} }
+		// Check Right
+		if (x < width - 1) { if (grid[x + 1][y] == true) {count++;} }
+		// Check Bottom-Right
+		if (x < width - 1 && y < height - 1) { if (grid[x + 1][y + 1] == true) {count++;} }
+		// Check Bottom
+		if (y < height - 1) { if (grid[x][y + 1] == true) {count++;} }
+		// Check Bottom-Left
+		if (x > 0 && y < height - 1) { if (grid[x - 1][y + 1] == true) {count++;} }
+
+		return count;
+	}
+
+	public boolean getCellState(int x, int y) {
+		return grid[x][y];
+	}
+
 }
