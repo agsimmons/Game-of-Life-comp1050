442c36a180ef6c530aa0c4cbf57b6822da824e89
Andrew Simmons <agsimmons0@gmail.com>
2016-03-30 12:18:01 -0400

    Create part of load and save method

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,6 +18,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -157,11 +159,51 @@ public class Main extends Application { // All the usual JavaFX stuffs
 	}
 
 	public void save(ActionEvent e) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Create saved state");
+		//File stateFile = fileChooser.showOpenDialog(null);
 		
+		//TODO: Save state of grid variable into file.
 	}
 
 	public void load(ActionEvent e) {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open saved state");
+		File stateFile = fileChooser.showOpenDialog(null);
 		
+		int saveWidth;
+		int saveHeight;
+		Scanner saveScanner = null;
+		
+		try {
+			System.out.println("Ding");
+			saveScanner = new Scanner(stateFile);
+		} catch (FileNotFoundException e1) {
+			System.out.println("ERROR: File Not Found!");
+			e1.printStackTrace();
+		}
+		
+		//TODO: Load contents of file into grid variable, then redraw(?)
+		try {
+			System.out.println("Dong");
+
+			saveWidth = saveScanner.nextInt();
+			saveHeight = saveScanner.nextInt();
+
+			boolean[][] tempGridState = new boolean[saveWidth][saveHeight];
+
+			while(saveScanner.hasNextInt()) {
+				tempGridState[saveScanner.nextInt()][saveScanner.nextInt()] = true;
+			}
+
+			baseGrid.setGridState(tempGridState);
+
+			//TODO: Draw new grid to gui
+
+		} catch(Exception e2) {
+			System.out.println("ERROR: Invalid File!");
+			e2.printStackTrace();
+		}
 	}
 
 	public void setRules(ActionEvent e) {
